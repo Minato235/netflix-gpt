ae1d0cd2268ad96f0420b8b3ff30afb8ae9a8bcc
Minato235 <ankamasish@gmail.com>
2024-09-08 18:14:00 -0400

    start

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -4,12 +4,13 @@ import { useNavigate } from "react-router-dom";
 import LeavingSoonPopup from "./LeavingSoon";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../util/userSlice";
-import { useDispatch } from "react-redux";
-import { logo,netflixLogo } from "../util/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { logo, netflixLogo } from "../util/constants";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user); // Access user from Redux store
 
   const handleSignOut = () => {
     signOut(auth)
@@ -22,6 +23,11 @@ const Header = () => {
         console.log("its happend error");
       });
   };
+
+  const handleToogle=()=>{
+    
+  }
+
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -46,32 +52,30 @@ const Header = () => {
     });
   }, [dispatch, navigate]);
 
-  
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-r  from-transparent to-black p-2 flex justify-between z-10">
-    <div>
-      <img
-        className="w-44"
-        src={netflixLogo}
-        alt="Logo"
-      />
+      <div>
+        <img className="w-44" src={netflixLogo} alt="Logo" />
+      </div>
+      {user && (
+        <div className="flex items-center">
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-7 py-3 text-center me-2 mb-2"
+          onClick={handleToogle}>
+            Netflix GPT
+          </button>
+          <img className="w-14 h-14 mx-4" src={logo} alt="icon-right" />
+          <button
+            onClick={handleSignOut}
+            onDoubleClick={LeavingSoonPopup}
+            className="font-bold mr-2 bg-slate-200 p-2 rounded"
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
-    <div className="flex items-center">
-      <img
-        className="w-14 h-14 mx-4"
-        src={logo}
-        alt="icon-right"
-      />
-      <button
-        onClick={handleSignOut}
-        onDoubleClick={LeavingSoonPopup}
-        className="font-bold mr-2 bg-slate-200 p-2 rounded"
-      >
-        Sign Out
-      </button>
-    </div>
-  </div>
-  
   );
 };
 export default Header;
