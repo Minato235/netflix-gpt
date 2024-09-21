d76386e5dbd4d2aea741759b0dd186b0ac5d10d5
Minato235 <ankamasish@gmail.com>
2024-09-21 09:49:16 -0400

    adding extra

--- src/util/openai.js
+++ src/util/openai.js
@@ -1,9 +1,9 @@
-import OpenAI from 'openai';
-import { netflix_gpt_key } from './constants';
+// import OpenAI from 'openai';
+// // import { netflix_gpt_key } from './constants';
 
-const openAi = new OpenAI({
-  apiKey: netflix_gpt_key ,
-  dangerouslyAllowBrowser:true
-  // This is the default and can be omitted
-});
-export default openAi
+// const openAi = new OpenAI({
+//   apiKey: netflix_gpt_key ,
+//   dangerouslyAllowBrowser:true
+//   // This is the default and can be omitted
+// });
+// export default openAi
